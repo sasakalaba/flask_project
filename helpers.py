20d1e860af679e54cc74604ad8df6a400503aabf
sasa.kalaba <sasabackup@yahoo.com>
2016-10-10 22:06:04 +0200

    Polished out helpers.

--- helpers.py
+++ helpers.py
@@ -5,8 +5,9 @@ from requests.compat import OrderedDict
 from requests.cookies import RequestsCookieJar
 from stormpath.auth import Sauthc1Signer
 from stormpath.client import Client
-from stormpath.resources import FactorList
+from stormpath.resources import FactorList, Factor
 from pydispatch import dispatcher
+from pprint import pprint
 
 
 def generate_request(method, url, body):
@@ -100,15 +101,22 @@ def assert_request(req, resp, expected_req, expected_resp):
     """
     Make sure that the request and response match the one we are trying to get.
     """
+    def assert_values(value1, value2, value_info):
+        if value1 != value2:
+            error_msg = '%s != %s' % (str(value1), str(value2))
+            raise AssertionError({'error': error_msg, 'value': value_info})
+
     def assert_headers(headers, expected_headers):
-        x_date = headers.pop('expected_value')
-        x_date_expected = expected_headers.pop('expected_value')
+        x_date = headers.pop('X-Stormpath-Date')
+        x_date_expected = expected_headers.pop('X-Stormpath-Date')
         auth = headers.pop('Authorization')
         auth_expected = expected_headers.pop('Authorization')
+        headers.pop('User-Agent')
+        expected_headers.pop('User-Agent')
 
-        assert len(x_date) == len(x_date_expected)
-        assert len(auth) == len(auth_expected)
-        assert headers == expected_headers
+        assert_values(len(x_date), len(x_date_expected), 'x_date')
+        assert_values(len(auth), len(auth_expected), 'auth')
+        assert_values(headers, expected_headers, 'headers')
 
     # Compare requests
     attrs = ['url', 'method', 'body']
@@ -118,20 +126,87 @@ def assert_request(req, resp, expected_req, expected_resp):
         expected_value = getattr(expected_req, attr)
 
         if attr == 'body':
-            assert json.loads(value) == json.loads(expected_value)
-        elif attr == 'headers':
-            assert_headers(value, expected_value)
+            assert_values(json.loads(value), json.loads(expected_value), attr)
         else:
-            assert value == expected_value
+            assert_values(value, expected_value, attr)
+
+    assert_headers(req.headers, expected_req.headers)
 
     # Compare responses
-    attrs = ['url', 'status_code', 'content']
+    attrs = ['url', 'status_code']
 
     for attr in attrs:
         value = getattr(resp, attr)
         expected_value = expected_resp.get(attr)
 
-        if attr == 'content':
-            assert json.loads(value) == expected_value
-        else:
-            assert value == expected_value
+        assert_values(value, expected_value, attr)
+
+    print '\n'
+    pprint(json.loads(resp.content))
+    print '\n'
+
+    return None
+
+
+def delete_resource(client, acc_url, resource):
+    client.accounts.get(acc_url).factors.items[0]
+    res_list = getattr(client.accounts.get(acc_url), resource + 's').items
+    if len(res_list) == 1:
+        res_list[0].delete()
+    else:
+        raise Exception('Resource cannot be deleted.')
+
+    return None
+
+
+def main():
+    # Additional imports
+    import os
+
+    # Reset flag
+    os.environ['MYFLAG'] = 'notbar'
+
+    # Set variables
+    resource_name = 'factor'
+    acc_url = 'https://api.stormpath.com/v1/accounts/6FZ1uG8uXob6TrxJJQB9j2'
+    method = 'POST'
+    url = 'https://api.stormpath.com/v1/accounts/6FZ1uG8uXob6TrxJJQB9j2/factors?challenge=true'
+    body = {
+        "phone": {"number": "+385 995734532"},
+        "challenge": {"message": "${code}"},
+        "type": "SMS"}
+    status_code = 201
+
+    expected_response = {
+        'url': url,
+        'status_code': status_code}
+
+    # Environ variables
+    os.environ['expected_resp'] = json.dumps(expected_response)
+    os.environ['exp_req_method'] = method
+    os.environ['exp_req_url'] = url
+    os.environ['exp_req_body'] = json.dumps(body)
+
+    # Main
+    resources = get_resources()
+    client = resources['client']
+
+    # Set flag
+    os.environ['MYFLAG'] = 'foobar'
+
+    try:
+        created = eval(resource_name.title())(
+            client,
+            properties=client.data_store.create_resource(
+                url, body, params=None))
+    except Exception as error:
+        os.environ['MYFLAG'] = 'notbar'
+        delete_resource(client, acc_url, resource_name)
+        return error
+
+    # Reset flag
+    os.environ['MYFLAG'] = 'notbar'
+
+    delete_resource(client, acc_url, resource_name)
+    print '\nSUCCESS\n'
+    return None
